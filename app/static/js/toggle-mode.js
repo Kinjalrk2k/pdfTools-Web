ac18f5cd68cc9aa2f29f6c571698e2e4462d9169
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-11-02 20:58:41 +0530

    added sticky navbar

--- app/static/js/toggle-mode.js
+++ app/static/js/toggle-mode.js
@@ -3,10 +3,12 @@ document.getElementById("toggle-mode-btn").addEventListener("click", () => {
   if (html.classList.contains("spectrum--light")) {
     html.classList.remove("spectrum--light");
     html.classList.add("spectrum--dark");
+    document.querySelector("#toggle-mode-btn span i").className = "fas fa-sun";
     localStorage.setItem("theme", "spectrum--dark");
   } else {
     html.classList.remove("spectrum--dark");
     html.classList.add("spectrum--light");
+    document.querySelector("#toggle-mode-btn span i").className = "fas fa-moon";
     localStorage.setItem("theme", "spectrum--light");
   }
 });
@@ -14,6 +16,12 @@ document.getElementById("toggle-mode-btn").addEventListener("click", () => {
 (function () {
   const onpageLoadTheme = localStorage.getItem("theme") || "spectrum--light";
   const html = document.getElementsByTagName("html")[0];
+
+  if (onpageLoadTheme === "spectrum--light") {
+    document.querySelector("#toggle-mode-btn span i").className = "fas fa-moon";
+  } else {
+    document.querySelector("#toggle-mode-btn span i").className = "fas fa-sun";
+  }
   html.classList.remove("spectrum--light");
   html.classList.remove("spectrum--dark");
   html.classList.add(onpageLoadTheme);
