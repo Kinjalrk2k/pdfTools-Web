2765d7fb408434e50e83e74921333d0c931bcae4
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-11-03 11:44:03 +0530

    added different upload directory logic

--- app/merger/controller.py
+++ app/merger/controller.py
@@ -1,7 +1,8 @@
+from flask.helpers import url_for
 from flask.templating import render_template
 from werkzeug.utils import secure_filename
 from app import merger
-from flask import Blueprint, request, current_app
+from flask import Blueprint, request, current_app, redirect
 import os
 import time
 
@@ -14,10 +15,15 @@ def index():
     return render_template('index.html.j2')
 
 
-@merger_blueprint.route('/upload', methods=['GET', 'POST'])
-def upload():
-    curr_time = str(int(time.time()))
-    folder = current_app.config['UPLOAD_FOLDER'] + curr_time
+@merger_blueprint.route('<folderid>/arrange/')
+def arrange(folderid):
+    print('here')
+    return render_template('arrange.html.j2', id=folderid)
+
+
+@merger_blueprint.route('<folderid>/upload', methods=['GET', 'POST'])
+def upload(folderid):
+    folder = current_app.config['UPLOAD_FOLDER'] + folderid
     if request.method == 'POST':
         if not os.path.exists(folder):
             os.makedirs(folder)
@@ -26,5 +32,6 @@ def upload():
             if key.startswith('file'):
                 f.save(os.path.join(folder, f.filename))
                 print("Uploaded...", f.filename)
-
-    return render_template('upload.html.j2')
+        return render_template('arrange.html.j2', id=folderid)
+    else:
+        return render_template('upload.html.j2', id=folderid)
