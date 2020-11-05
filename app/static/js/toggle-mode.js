9d31faeedb134ddfbc63319e27f8af17b666ab99
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-11-05 23:16:40 +0530

    added gifs

--- app/static/js/toggle-mode.js
+++ app/static/js/toggle-mode.js
@@ -1,15 +1,26 @@
 document.getElementById("toggle-mode-btn").addEventListener("click", () => {
   const html = document.getElementsByTagName("html")[0];
   if (html.classList.contains("spectrum--light")) {
+    localStorage.setItem("theme", "spectrum--dark");
     html.classList.remove("spectrum--light");
     html.classList.add("spectrum--dark");
     document.querySelector("#toggle-mode-btn span i").className = "fas fa-sun";
-    localStorage.setItem("theme", "spectrum--dark");
+
+    if (document.getElementsByClassName("tutorial-img-row").length > 0) {
+      document.getElementById("tutorial-img-light").style.display = "none";
+      document.getElementById("tutorial-img-dark").style.display = "block";
+    }
   } else {
+    localStorage.setItem("theme", "spectrum--light");
+
     html.classList.remove("spectrum--dark");
     html.classList.add("spectrum--light");
     document.querySelector("#toggle-mode-btn span i").className = "fas fa-moon";
-    localStorage.setItem("theme", "spectrum--light");
+
+    if (document.getElementsByClassName("tutorial-img-row").length > 0) {
+      document.getElementById("tutorial-img-dark").style.display = "none";
+      document.getElementById("tutorial-img-light").style.display = "block";
+    }
   }
 });
 
@@ -18,8 +29,18 @@ document.getElementById("toggle-mode-btn").addEventListener("click", () => {
   const html = document.getElementsByTagName("html")[0];
 
   if (onpageLoadTheme === "spectrum--light") {
+    if (document.getElementsByClassName("tutorial-img-row").length > 0) {
+      document.getElementById("tutorial-img-dark").style.display = "none";
+      document.getElementById("tutorial-img-light").style.display = "block";
+    }
+
     document.querySelector("#toggle-mode-btn span i").className = "fas fa-moon";
   } else {
+    if (document.getElementsByClassName("tutorial-img-row").length > 0) {
+      document.getElementById("tutorial-img-light").style.display = "none";
+      document.getElementById("tutorial-img-dark").style.display = "block";
+    }
+
     document.querySelector("#toggle-mode-btn span i").className = "fas fa-sun";
   }
   html.classList.remove("spectrum--light");
