b69d7072cb328b7d75a2fc1ba78b0b81de0a195e
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-11-06 20:55:45 +0530

    added front-end countddown

---
+++ app/static/js/countdown.js
@@ -0,0 +1,20 @@
+const zeroPad = (num, places) => String(num).padStart(places, "0");
+
+function countdown_wrapper(id) {
+  setInterval(function () {
+    const start_time = Number(id);
+    const end_time = start_time + 600;
+    const curr_time = Math.floor(Date.now() / 1000);
+    const delta = end_time - curr_time;
+
+    if (delta <= 0) {
+      window.location.href = "/session-expired";
+    }
+
+    const minutes = Math.floor((delta % (60 * 60)) / 60);
+    const seconds = Math.floor(delta % 60);
+
+    console.log(minutes + ":" + seconds);
+    document.getElementById("countdown").innerHTML = zeroPad(minutes, 2) + ":" + zeroPad(seconds, 2);
+  }, 1000);
+}
