dddf801ca6e5edaa2feb3086cf7e47a0a4b85839
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-11-07 18:03:56 +0530

    major bug fix :"(

--- app/utils/metadata.py
+++ app/utils/metadata.py
@@ -1,28 +1,22 @@
 def build_metadata(formdata):
     i = 0
-    ignore = False
     temp_dict = dict()
     ordered_file_list = []
 
     for key, value in formdata.items():
         print(i, key, value)
         if i == 0:
-            if value == "on":
-                ignore = False
-            else:
-                ignore = True
-                i += 1
-        if i == 1 and (not ignore):
+            if not value == "on":
+                continue
+        if i == 1:
             temp_dict["filename"] = value
-        elif i == 2 and (not ignore):
+        elif i == 2:
             temp_dict["start"] = int(value)
         elif i == 3:
-            if not ignore:
-                temp_dict["end"] = int(value)
-                # print(temp_dict)
-                ordered_file_list.append(temp_dict.copy())
+            temp_dict["end"] = int(value)
+            print(temp_dict)
+            ordered_file_list.append(temp_dict.copy())
             i = 0
-            ignore = False
             continue
         i += 1
 
